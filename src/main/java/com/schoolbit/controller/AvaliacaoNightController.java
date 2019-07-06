62f1c7777f78120c99c1969c527185ae0decf91e
Cabral <charles.r.cabral@accenture.com>
2019-07-06 17:16:41 -0300

    implementacao buscar por Id e excluir por Id

--- src/main/java/com/schoolbit/controller/AvaliacaoNightController.java
+++ src/main/java/com/schoolbit/controller/AvaliacaoNightController.java
@@ -3,27 +3,35 @@ package com.schoolbit.controller;
 import com.schoolbit.model.AvaliacaoNight;
 import com.schoolbit.service.AvaliacaoNightService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/avaliacaonight")
 public class AvaliacaoNightController {
 
     @Autowired
     private AvaliacaoNightService avaliacaoNightService;
 
-    @PostMapping("/avaliacoes")
+    @PostMapping
     public void salvar(@RequestBody AvaliacaoNight avaliacaoNight){
 
         avaliacaoNightService.salvar(avaliacaoNight);
     }
 
-    @GetMapping("/avaliacoes")
+    @GetMapping
     public List<AvaliacaoNight> buscarTodas(){
         return avaliacaoNightService.buscarTodas();
+    }
+
+    @GetMapping("/{id}")
+    public AvaliacaoNight buscarPorId(@PathVariable("id") Long id) {
+        return avaliacaoNightService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirPorId(@PathVariable("id") Long id) {
+        this.avaliacaoNightService.excluirPorId(id);
     }
 }
