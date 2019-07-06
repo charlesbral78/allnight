d13ae5dfdfacc74d51192d0ae07d50385c3f487e
Silvio Marques <silviotec.marques@gmail.com>
2019-07-06 14:25:52 -0300

    Api_De_Estilo

---
+++ src/main/java/com/schoolbit/repository/EstiloRepository.java
@@ -0,0 +1,10 @@
+package com.schoolbit.repository;
+
+
+import com.schoolbit.model.Estilo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstiloRepository extends JpaRepository<Estilo, Long> {
+}
