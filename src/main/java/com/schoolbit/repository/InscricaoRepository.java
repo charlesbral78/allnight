cdc95d8ccf6f02d557266f94cbb1796003a48049
charlesbral78 <charlesbral78@hotmmail.com>
2019-07-13 20:19:32 -0300

    remocao dos metodos que nao estavao sendo usados

--- src/main/java/com/schoolbit/repository/InscricaoRepository.java
+++ src/main/java/com/schoolbit/repository/InscricaoRepository.java
@@ -1,7 +1,5 @@
 package com.schoolbit.repository;
 
-
-import com.schoolbit.constants.FormaPagamento;
 import com.schoolbit.model.Inscricao;
 import com.schoolbit.model.Night;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +10,5 @@ import java.util.List;
 @Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
 
-    List<Inscricao> findByFormaPagamento(FormaPagamento formaPagamento);
-
-    List<Inscricao> findByNumeroCartao(String numeroCartao);
-
     List<Inscricao> findByNight(Night night);
-
 }
